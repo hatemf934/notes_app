88ba3bedecec8b40e98afa5a9221b129b96f6c68
hatemfathy <hatemf934@gmail.com>
2025-02-08 00:46:41 +0200

    create note model

---
+++ lib/models/model_notes.dart
@@ -0,0 +1,11 @@
+class ModelNotes {
+  final String title;
+  final String subtitle;
+  final DateTime date;
+  final int color;
+  ModelNotes(
+      {required this.title,
+      required this.subtitle,
+      required this.date,
+      required this.color});
+}
