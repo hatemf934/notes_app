aea88237c1cd18d4f52646969b68df0a227d9eff
hatemfathy <hatemf934@gmail.com>
2025-02-08 14:08:30 +0200

    add buildrunner and hivegenerator and create modelnotes_generator

--- lib/models/model_notes.dart
+++ lib/models/model_notes.dart
@@ -1,7 +1,15 @@
-class ModelNotes {
+import 'package:hive/hive.dart';
+part 'model_notes.g.dart';
+
+@HiveType(typeId: 0)
+class ModelNotes extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final DateTime date;
+  @HiveField(3)
   final int color;
   ModelNotes(
       {required this.title,
