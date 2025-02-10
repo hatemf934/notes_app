495f824a170dfdb779b8be857589fcc5aa766e29
hatemfathy <hatemf934@gmail.com>
2025-02-10 02:07:26 +0200

    add notes_cubit to show_model

--- lib/widgets/custom_show_model_bottomsheet.dart
+++ lib/widgets/custom_show_model_bottomsheet.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/addNotesCubit/add_note_cubit.dart';
+import 'package:note_app/helper/snakbar.dart';
 import 'package:note_app/widgets/form_note_view.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,8 +12,23 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(16),
-      child: const SingleChildScrollView(
-        child: NoteFormView(),
+      child: SingleChildScrollView(
+        child: BlocConsumer<NotesCubit, NotesState>(
+          listener: (context, state) {
+            if (state is NotesSuccess) {
+              Navigator.pop(context);
+            }
+            if (state is NotesFailure) {
+              snackBar(context, state.errormessage);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is NotesLoading ? true : false,
+              child: const NoteFormView(),
+            );
+          },
+        ),
       ),
     );
   }
